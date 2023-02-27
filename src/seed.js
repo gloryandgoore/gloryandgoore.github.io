82386b60d832493b0b3df65ee1905d3d5ee36a63
Caseyann Goore <goore_44@hotmail.ca>
2023-02-26 23:28:25 -0500

    setup gh-pages

---
+++ src/seed.js
@@ -0,0 +1,55 @@
+import shortid from "shortid";
+
+export default store => {
+  console.log("Insert first list");
+  const firstListId = shortid.generate();
+
+  store.dispatch({
+    type: "ADD_LIST",
+    payload: { listId: firstListId, listTitle: "BackLog" }
+  });
+
+  store.dispatch({
+    type: "ADD_CARD",
+    payload: {
+      listId: firstListId,
+      cardId: shortid.generate(),
+      cardText: "Add Stying"
+    }
+  });
+
+  store.dispatch({
+    type: "ADD_CARD",
+    payload: {
+      listId: firstListId,
+      cardId: shortid.generate(),
+      cardText: "Refactor code"
+    }
+  });
+
+  console.log("Insert second list");
+  const secondListId = shortid.generate();
+
+  store.dispatch({
+    type: "ADD_LIST",
+    payload: { listId: secondListId, listTitle: "In Progress" }
+  });
+
+  store.dispatch({
+    type: "ADD_CARD",
+    payload: {
+      listId: secondListId,
+      cardId: shortid.generate(),
+      cardText: "add css"
+    }
+  });
+
+  store.dispatch({
+    type: "ADD_CARD",
+    payload: {
+      listId: secondListId,
+      cardId: shortid.generate(),
+      cardText: "add data"
+    }
+  });
+};
